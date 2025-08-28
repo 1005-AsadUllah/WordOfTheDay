07cb392dd7d30a4d9a924e3b98e61d3c8e339a0e
Asad_Ullah <au10050305@gmail.com>
2025-08-28 10:39:05 +0600

    Initial Commit

---
+++ src/test/java/com/AsadUllah/WordOfTheDay/WordOfTheDayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AsadUllah.WordOfTheDay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordOfTheDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
