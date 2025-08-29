4b281e8a339e065892008ca11fb4e34879b0f095
Asad_Ullah <au10050305@gmail.com>
2025-08-29 21:51:37 +0600

    Remove unused imports from WordEntity and WordRepository classes

--- src/main/java/com/AsadUllah/WordOfTheDay/repository/WordRepository.java
+++ src/main/java/com/AsadUllah/WordOfTheDay/repository/WordRepository.java
@@ -3,9 +3,8 @@ package com.AsadUllah.WordOfTheDay.repository;
 import com.AsadUllah.WordOfTheDay.entity.WordEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDate;
 import java.util.Optional;
 
-public interface WordRepository extends JpaRepository<WordEntity,Long> {
+public interface WordRepository extends JpaRepository<WordEntity, Long> {
     Optional<WordEntity> findByWord(String word);
 }
