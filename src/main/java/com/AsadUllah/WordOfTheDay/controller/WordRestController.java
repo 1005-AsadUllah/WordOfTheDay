bb950035eeeafa9e750e95227274c93ccc603ed6
Asad_Ullah <au10050305@gmail.com>
2025-08-29 19:23:08 +0600

    Add Swagger dependency for documentation; document WordRestController endpoint

--- src/main/java/com/AsadUllah/WordOfTheDay/controller/WordRestController.java
+++ src/main/java/com/AsadUllah/WordOfTheDay/controller/WordRestController.java
@@ -2,6 +2,7 @@ package com.AsadUllah.WordOfTheDay.controller;
 
 import com.AsadUllah.WordOfTheDay.Model.WordResponse;
 import com.AsadUllah.WordOfTheDay.service.WordService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ public class WordRestController {
 
     private final WordService wordService;
 
+    @Operation(summary = "Get the word of the day with its definitions and parts of speech")
     @GetMapping("/wordOfTheDay")
     public WordResponse getWord() {
         return wordService.getWord();
